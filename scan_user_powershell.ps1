fb9ee040f289dc833bdc274b8a28f3b30f36a8f2
Noah Callahan <36965518+noah-callahan@users.noreply.github.com>
2018-12-13 23:27:22 -0500

    Update scan_user_powershell.ps1

--- scan_user_powershell.ps1
+++ scan_user_powershell.ps1
@@ -1,10 +1,16 @@
-"Enter password for scans user: "
+"Enter password for scans user. Enter d for default 'Scanner#1' password: "
 $Password = Read-Host -AsSecureString
 
 New-LocalUser -Name "scans" -Password $Password -Description "account for scanning" -PasswordNeverExpires -AccountNeverExpires
 
-md C:\scans
+$Destination = $Env:userprofile
+$main_dir = ($Destination[0..2] -join "")
 
-New-SmbShare -Name "scans" -path "C:\scans" -FullAccess scans
+$scan_folder = $main_dir + "scans"
+
+mkdir $scan_folder
+
+New-SmbShare -Name "scans" -path $scan_folder -FullAccess scans
 
 Read-Host -Prompt "Press Enter to exit"
+
