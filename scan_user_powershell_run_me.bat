db7eeab9f8d5068023ce3a579dd48bcf4263d193
Noah Callahan <36965518+noah-callahan@users.noreply.github.com>
2018-12-16 18:59:21 -0500

    Update scan_user_powershell_run_me.bat

--- scan_user_powershell_run_me.bat
+++ scan_user_powershell_run_me.bat
@@ -1,1 +1,4 @@
-PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Unrestricted -File "C:\networkscan\scan_user_powershell.ps1"' -Verb RunAs}";
+@ECHO OFF
+SET ThisScriptsDirectory=%~dp0
+SET PowerShellScriptPath=%ThisScriptsDirectory%scan_user_powershell.ps1
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%PowerShellScriptPath%""' -Verb RunAs}"
